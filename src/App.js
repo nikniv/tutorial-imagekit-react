c1272f4729fb6b512974dcd03b84cbf1e0ca48c3
nikniv <niveditn@gmail.com>
2020-09-21 07:20:08 +0530

    commit frontend sample app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to ImageKit React SDK Tutorial!
+    </div>
+  );
+}
+
+export default App;
+
